d3f07ceb735ede08cc3c771828e5c6a23474d264
Gabriel Oliveira <gabrielglvr@gmail.com>
2019-06-05 09:52:09 -0300

    Add basic routes and files, changes in initial structure

--- src/containers/Routes/index.js
+++ src/containers/Routes/index.js
@@ -1,8 +1,13 @@
 import MovieContainer from '../Movie/MovieContainer'
+import MovieDetailsContainer from '../Movie/MovieDetailsContainer'
 
 export default [
     {
         component: MovieContainer,
         path: '/',
+    },
+    {
+        component: MovieDetailsContainer,
+        path: '/:id'
     }
 ];
