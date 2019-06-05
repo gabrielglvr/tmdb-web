a833a56f613f202f88a446bde661e3a951a8f76d
Gabriel Oliveira <c1299745@bb.com.br>
2019-06-05 14:51:24 -0300

    Add enviroments

--- src/actions/MovieActions.js
+++ src/actions/MovieActions.js
@@ -2,7 +2,7 @@ import { GET_UPCOMING_MOVIES, SEARCH_MOVIE, GET_MOVIE_BY_ID,
     SET_CURRENT_PAGE, SET_SEARCH_TITLE } from './ActionTypes'
 import axios from 'axios';
 
-const baseURl = 'http://localhost:5000/api/v1';
+const baseURl = process.env.REACT_APP_API_BASE_URL;
 const upcomingMoviesUrl = baseURl + '/movie/upcoming';
 const movieUrl = baseURl + '/movie';
 
